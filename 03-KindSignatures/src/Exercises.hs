0e1635344e20cb5a0b3ac871e2c9265965ea15c0
Flavio Corpa <flavio.corpa@47deg.com>
2020-08-21 12:09:54 +0200

    Solve exercises for KindSignatures! 😇

--- 03-KindSignatures/src/Exercises.hs
+++ 03-KindSignatures/src/Exercises.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE KindSignatures    #-}
+{-# LANGUAGE KindSignatures #-}
+
 module Exercises where
 
 import Data.Kind (Constraint, Type)
@@ -8,95 +9,65 @@ import Data.Kind (Constraint, Type)
 -- will tell you exactly what the problem is, so be sure to try /before/
 -- uncommenting!
 
-
-
-
-
 {- ONE -}
 
-class Question1 a
--- instance Question1 Maybe
+class Question1 (a :: Type -> Type)
 
-
-
-
+instance Question1 Maybe
 
 {- TWO -}
 
-class Question2 a
--- instance Question2 Either
+class Question2 (a :: Type -> Type -> Type)
 
-
-
-
+instance Question2 Either
 
 {- THREE -}
 
-class Question3 a
--- instance Question3 (,,)
+class Question3 (a :: Type -> Type -> Type -> Type)
 
-
-
-
+instance Question3 (,,)
 
 {- FOUR -}
 
-class Question4 a
--- instance Question4 (->)
+class Question4 (a :: Type -> Type -> Type)
 
-
-
-
+instance Question4 (->)
 
 {- FIVE -}
 
 class Question5 a
--- instance Question5 [[Int]]
 
-
-
-
+instance Question5 [[Int]]
 
 {- SIX -}
 
 class Question6 a
--- instance Question6 (Either () (Maybe (IO String)))
 
-
-
-
+instance Question6 (Either () (Maybe (IO String)))
 
 {- SEVEN -}
 
-class Question7 a
--- instance Question7 (Eq Int) -- This instance looks flexible...
+class Question7 (a :: Constraint)
 
-
-
-
+instance Question7 (Eq Int) -- This instance looks flexible...
 
 {- EIGHT -}
 
-class Question8 a
--- instance Question8 Show
+class Question8 (a :: Type -> Constraint)
 
-
-
-
+instance Question8 Show
 
 {- NINE -}
 
-class Question9 a
--- instance Question9 (Functor Maybe)
+class Question9 (a :: Constraint)
 
-
-
-
+instance Question9 (Functor Maybe)
 
 {- TEN -}
 
 -- A kind error! Assume two out of three of these have the correct kind; what
 -- should it be? Which part is broken? What's wrong with it?
 
-class Question10 a
--- instance Question10 (Monad m, Eq m, Show a)
+class Question10 (a :: Constraint)
+
+instance Question10 (Monad m, Eq (m a), Show a)
