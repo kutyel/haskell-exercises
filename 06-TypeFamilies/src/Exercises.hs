979e0011b134b6bce6780969e7b27661d2a09e32
Flavio Corpa <flavio.corpa@47deg.com>
2021-01-04 14:40:11 +0100

    Solve 5/9 exercises with TypeFamilies!

--- 06-TypeFamilies/src/Exercises.hs
+++ 06-TypeFamilies/src/Exercises.hs
@@ -1,7 +1,9 @@
-{-# LANGUAGE DataKinds     #-}
-{-# LANGUAGE GADTs         #-}
-{-# LANGUAGE TypeFamilies  #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module Exercises where
 
 import Data.Kind (Constraint, Type)
@@ -11,110 +13,118 @@ import Data.Kind (Constraint, Type)
 -- regular names as infix names with the backticks, as we would at the value
 -- level.
 
-
-
-
-
 {- ONE -}
 
 data Nat = Z | S Nat
 
 -- | a. Use the @TypeOperators@ extension to rewrite the 'Add' family with the
 -- name '+':
+type family (+) (a :: Nat) (b :: Nat) :: Nat where
+  'Z + y = y
+  'S x + y = 'S (x + y)
 
 -- | b. Write a type family '**' that multiplies two naturals using '(+)'. Which
--- extension are you being told to enable? Why?
+-- extension are you being told to enable? Why? 🤷🏻‍♂️
+type family (**) (a :: Nat) (b :: Nat) :: Nat where
+  'Z ** _ = 'Z
+  'S x ** y = y + (x ** y)
 
+-- | c. Write a function to add two 'SNat' values.
 data SNat (value :: Nat) where
   SZ :: SNat 'Z
   SS :: SNat n -> SNat ('S n)
 
--- | c. Write a function to add two 'SNat' values.
-
-
-
-
+add :: SNat x -> SNat y -> SNat (x + y)
+add SZ y = y
+add (SS x) y = SS (add x y)
 
 {- TWO -}
 
 data Vector (count :: Nat) (a :: Type) where
-  VNil  :: Vector 'Z a
+  VNil :: Vector 'Z a
   VCons :: a -> Vector n a -> Vector ('S n) a
 
 -- | a. Write a function that appends two vectors together. What would the size
 -- of the result be?
-
--- append :: Vector m a -> Vector n a -> Vector ??? a
+append :: Vector m a -> Vector n a -> Vector (m + n) a
+append VNil ws = ws
+append (VCons v vs) ws = VCons v (append vs ws)
 
 -- | b. Write a 'flatMap' function that takes a @Vector n a@, and a function
 -- @a -> Vector m b@, and produces a list that is the concatenation of these
 -- results. This could end up being a deceptively big job.
-
--- flatMap :: Vector n a -> (a -> Vector m b) -> Vector ??? b
-flatMap = error "Implement me!"
-
-
-
-
+flatMap :: Vector n a -> (a -> Vector m b) -> Vector (n ** m) b
+flatMap VNil _ = VNil
+flatMap (VCons x xs) f = f x `append` flatMap xs f
 
 {- THREE -}
 
 -- | a. More boolean fun! Write the type-level @&&@ function for booleans.
+type family (&&) (a :: Bool) (b :: Bool) :: Bool where
+  'True && 'True = 'True
+  _ && _ = 'False
 
 -- | b. Write the type-level @||@ function for booleans.
+type family (||) (a :: Bool) (b :: Bool) :: Bool where
+  'False || x = x
+  'True || _ = 'True
 
 -- | c. Write an 'All' function that returns @'True@ if all the values in a
 -- type-level list of boleans are @'True@.
-
-
-
-
+type family All (xs :: [Bool]) :: Bool where
+  All '[] = 'True
+  All (x ': xs) = x && All xs
 
 {- FOUR -}
 
 -- | a. Nat fun! Write a type-level 'compare' function using the promoted
 -- 'Ordering' type.
+type family Compare (x :: Nat) (y :: Nat) :: Ordering where
+  Compare 'Z 'Z = 'EQ
+  Compare 'Z ('S _) = 'LT
+  Compare ('S _) 'Z = 'GT
+  Compare ('S x) ('S y) = Compare x y
 
 -- | b. Write a 'Max' family to get the maximum of two natural numbers.
+type family Max (x :: Nat) (y :: Nat) :: Nat where
+  Max x y = Max' (Compare x y) x y
+
+type family Max' (o :: Ordering) (x :: Nat) (y :: Nat) :: Nat where
+  Max' 'GT x _ = x
+  Max' _ _ x = x
 
 -- | c. Write a family to get the maximum natural in a list.
-
-
-
-
+type family Maximum (x :: [Nat]) :: Nat where
+  Maximum '[] = 'Z
+  Maximum (x ': xs) = Max x (Maximum xs)
 
 {- FIVE -}
 
 data Tree = Empty | Node Tree Nat Tree
 
 -- | Write a type family to insert a promoted 'Nat' into a promoted 'Tree'.
+type family Insert (x :: Nat) (xs :: Tree) :: Tree where
+  Insert x 'Empty = 'Node 'Empty x 'Empty
+  Insert x ('Node l c r) = Insert' (Compare x c) x ('Node l c r)
 
-
-
-
+type family Insert' (o :: Ordering) (x :: Nat) (xs :: Tree) :: Tree where
+  Insert' 'LT x ('Node l c r) = 'Node (Insert x l) c r
+  Insert' 'GT x ('Node l c r) = 'Node l c (Insert x r)
+  Insert' 'EQ _ xs = xs
 
 {- SIX -}
 
 -- | Write a type family to /delete/ a promoted 'Nat' from a promoted 'Tree'.
 
-
-
-
-
 {- SEVEN -}
 
 -- | With @TypeOperators@, we can use regular Haskell list syntax on the
 -- type-level, which I think is /much/ tidier than anything we could define.
-
 data HList (xs :: [Type]) where
-  HNil  :: HList '[]
+  HNil :: HList '[]
   HCons :: x -> HList xs -> HList (x ': xs)
 
 -- | Write a function that appends two 'HList's.
-
-
-
-
 
 {- EIGHT -}
 
@@ -127,25 +137,20 @@ data HList (xs :: [Type]) where
 -- - Unlike tuples, constraints are "auto-flattened": ((a, b), (c, (d, ())) is
 --   exactly equivalent to (a, b, c, d). Thanks to this property, we can build
 --   up constraints using type families!
-
 type family CAppend (x :: Constraint) (y :: Constraint) :: Constraint where
   CAppend x y = (x, y)
 
 -- | a. Write a family that takes a constraint constructor, and a type-level
 -- list of types, and builds a constraint on all the types.
-
 type family Every (c :: Type -> Constraint) (x :: [Type]) :: Constraint where
-  -- ...
+
+-- ...
 
 -- | b. Write a 'Show' instance for 'HList' that requires a 'Show' instance for
 -- every type in the list.
 
 -- | c. Write an 'Eq' instance for 'HList'. Then, write an 'Ord' instance.
 -- Was this expected behaviour? Why did we need the constraints?
-
-
-
-
 
 {- NINE -}
 
